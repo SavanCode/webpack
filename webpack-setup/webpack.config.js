a3d5fddca7e21dda06f360a19be42281bdb5aaed
Savannah <71588465+SavanCode@users.noreply.github.com>
2021-06-05 12:45:06 +0800

    custom entry and output path

--- webpack-setup/webpack.config.js
+++ webpack-setup/webpack.config.js
@@ -1,12 +1,21 @@
 // this means for all js files(exclude node_modules)，
 //we would use babel loader to transpiling them by using babel-loader, 
 //which using the standard from babel./preset-env
+
+const path = require('path')
+
 module.exports = {
     mode: 'development',
     //mode: 'production', 
     devtool: 'source-map',
+    entry: './src/index.js',
+    output:{
+        filename:'bundle.js',
+        path: path.resolve(__dirname,'public') //because of different operating system,no hard coding
+    },
     devServer:{
-        contentBase:'./dist'
+        //contentBase:'./dist'
+        contentBase:'./public'//for part 10 custom output location
     },
     module:{
         rules:[ 
